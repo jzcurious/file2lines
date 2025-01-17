3ec13dbb7eb3b3c155a1dc0bfe6bee5a7a106c70
jzcurious <paranoid.hpc.io@gmail.com>
2025-01-18 00:43:49 +0700

    add samples.cpp

---
+++ samples/src/samples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <txt2lines/reader.hpp>
+
+static void print_txt_with_enumeration(const char* path_to_file) {
+  auto reader = t2l::Reader(path_to_file);
+
+  for (t2l::MaybeLine mline; not reader.eof();) {
+    mline = reader.read_line();
+    std::cout << mline.num << "  " << mline.content << std::endl;
+  }
+}
+
+int main() {
+  print_txt_with_enumeration("../../assets/tests.udl");
+  return (0);
+}
